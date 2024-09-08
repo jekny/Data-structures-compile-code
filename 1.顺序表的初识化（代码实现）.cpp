4301e4767cae26511580cee91250191220449e6e
jekny <146811463+jekny@users.noreply.github.com>
2024-09-08 15:21:03 +0800

    Update 1.顺序表的初识化（代码实现）.cpp
    添加了一些注释，修改了new student[MAXSIZE]：加了[MAXSIZE]。

--- 1.顺序表的初识化（代码实现）.cpp
+++ 1.顺序表的初识化（代码实现）.cpp
@@ -1,7 +1,7 @@
 //c语言初始化顺序表
 #include<stdio.h>
 #include<stdlib.h>
-#define MAXSIZE 100
+#define MAXSIZE 100  //注意宏定义后面不加分号
 typedef struct sd
 {
 	int age;
@@ -9,10 +9,10 @@ typedef struct sd
 }student;
 typedef struct sq
 {
-	student *data;
-	int length;
+	student *data;//基地址（首地址）
+	int length;   //记录表的长度（即元素个数）
 }sqList;
-sqList L;
+sqList L;//创建一个sqList类型的叫L的顺序表
 int InitList(sqList* L)//传递的是地址，所以用指针来接收
 {
 	L->data = (student*)malloc(sizeof(student) * MAXSIZE);
@@ -31,7 +31,7 @@ int main()
 
 
 //c++中初始化顺序表
-#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1（VS中的）
 #include<iostream>
 #include<cstdlib>
 using namespace std;
@@ -49,8 +49,10 @@ typedef struct sq
 }sqList;
 sqList L;
 int InitList(sqList& L)//这里的&是引用，在函数内部对表的操作会直接改变表本身；
-{
-	L.data = new student;
+{			      //这里new一块空间也有说法：对顺序表（sqList）的初始化只需要为 student 类型的数组分配内存，而不是为整个 sqList 结构体数组分配内存，因为我们顺序表中的
+			      //内容只包括age，name。我们都把它们放到了类型为student* 的data中，所以我们new一块空间只需要new student[MAXSIZE]的空间。length 成员是 sqList 结构体的一部分，它存储在栈上（如果
+			      //sqList 是在函数内部定义的局部变量的话）或者全局/静态存储区（如果 sqList 是全局变量或静态变量的话）。因此，length 不需要额外分配内存，它已经和 sqList 结构体的其他成员一起被分配了
+	L.data = new student[MAXSIZE]; //new一块空间
 	if (L.data == nullptr)//nullptr与NULL等价，只是c++中用
 	{
 		exit(-1);//退出程序函数
